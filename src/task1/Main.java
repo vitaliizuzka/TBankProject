dc9d47f82888b4319798f5475234027aec6f937d
vitaliizuzka <vitaliizuzka@gmail.com>
2025-05-16 13:02:12 +0300

    Update Main.java

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-
+        int x = 0;
         String answer = null;
         text = text.toUpperCase();
         answer = text.charAt(0) == 'R' || (text.charAt(1) == 'R' && text.charAt(2) == 'M') ? "Yes" : "No";
